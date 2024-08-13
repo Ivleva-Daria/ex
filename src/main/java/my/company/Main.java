4763859da5149fc3ceed82eebf05d54c9177cf30
Ivleva Daria <daraivleva296@gmail.com>
2024-08-13 12:13:45 +0300

    Рефакторинг кода.Добавление методов чтения readScanner и readBuffer.Добавление класса Tasks.

--- src/main/java/my/company/Main.java
+++ src/main/java/my/company/Main.java
@@ -1,82 +1,44 @@
 package my.company;
-
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner choice = new Scanner(System.in);
+        //Выбор номера заадачи
         System.out.print("Ввведите номер задачи: ");
-        int ch = choice.nextInt();
-        switch(ch) {
-            case 1: One one = new One();
-            one.methodOne();
-            break;
-            case 2: Two two = new Two();
-            two.methodTwo();
-            break;
-            case 3: Three three = new Three();
-            three.methodThree();
-            break;
-            case 4: Four four = new Four();
-            four.methodFour();
-            break;
-            case 5: Five five = new Five();
-            five.methodFive();
-            break;
-            case 6: Six six = new Six();
-            six.methodSix();
-            break;
-            case 7: Seven seven = new Seven();
-            seven.methodSeven();
-            break;
-            case 8: Eight eight = new Eight();
-            eight.methodEight();
-            break;
-            case 9: Nine nine = new Nine();
-            nine.methodNine();
-            break;
-            case 10: Ten ten = new Ten();
-            ten.methodTen();
-            break;
-            case 11: Eleven eleven = new Eleven();
-            eleven.methodEleven();
-            break;
-            case 12: Twelve twelve = new Twelve();
-            twelve.methodTwelve();
-            break;
-            case 13: Thirteen thirteen = new Thirteen();
-            thirteen.methodthirteen();
-            break;
-            case 14: Fourteen fourteen = new Fourteen();
-            fourteen.methodfourteen();
-            break;
-            case 15: Fifteen fifteen = new Fifteen();
-            fifteen.methodfifteen();
-            break;
-            case 16: Sixteen sixteen = new Sixteen();
-            sixteen.methodsixteen();
-            break;
-            case 17: Seventeen factorial = new Seventeen();
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Введите число n: ");
-            int n = sc.nextInt();
-            factorial.factorial(n);
-            break;
-            case 18: Eighteen dayOfTheWeek = new Eighteen();
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Введите номер дня недели: ");
-            int d = scanner.nextInt();
-            dayOfTheWeek.dayOfTheWeek(d);
-            break;
-            case 19: Nineteen numberWord = new Nineteen();
-            Scanner scanner1 = new Scanner(System.in);
-            System.out.print("Введите число: ");
-            int num = scanner1.nextInt();
-            numberWord.numberWord(num);
-            break;
-        }
+        //Выбор способа чтения
+        int chNew = methodRead();
+        //Работа задачи
+        Tasks tasks = new Tasks();
+        tasks.methodTask(chNew);
 
     }
+    //Методы чтения
+    private static int  methodRead() {
+        //int ch1 = readScanner();
+        int ch2 = readBuffer();
+        return ch2;
+    }
+    //Метод чтения Scanner
+    private static int readScanner(){
+        Scanner choice = new Scanner(System.in);
+        int ch1 = choice.nextInt();
+        return ch1;
+    }
+    //Метод чтения BufferedReader
+    private static int readBuffer(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String ch2 = reader.readLine();
+            return Integer.parseInt(ch2);
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода");
+            return -1;
+        }
+    }
+
 }
 
---
+++ src/main/java/my/company/Tasks.java
@@ -0,0 +1,95 @@
+package my.company;
+import java.util.Scanner;
+
+public class Tasks {
+    void methodTask(int chNew) {
+        switch(chNew) {
+            case 1:
+                One one = new One();
+                one.methodOne();
+                break;
+            case 2:
+                Two two = new Two();
+                two.methodTwo();
+                break;
+            case 3:
+                Three three = new Three();
+                three.methodThree();
+                break;
+            case 4:
+                Four four = new Four();
+                four.methodFour();
+                break;
+            case 5:
+                Five five = new Five();
+                five.methodFive();
+                break;
+            case 6:
+                Six six = new Six();
+                six.methodSix();
+                break;
+            case 7:
+                Seven seven = new Seven();
+                seven.methodSeven();
+                break;
+            case 8:
+                Eight eight = new Eight();
+                eight.methodEight();
+                break;
+            case 9:
+                Nine nine = new Nine();
+                nine.methodNine();
+                break;
+            case 10:
+                Ten ten = new Ten();
+                ten.methodTen();
+                break;
+            case 11:
+                Eleven eleven = new Eleven();
+                eleven.methodEleven();
+                break;
+            case 12:
+                Twelve twelve = new Twelve();
+                twelve.methodTwelve();
+                break;
+            case 13:
+                Thirteen thirteen = new Thirteen();
+                thirteen.methodthirteen();
+                break;
+            case 14:
+                Fourteen fourteen = new Fourteen();
+                fourteen.methodfourteen();
+                break;
+            case 15:
+                Fifteen fifteen = new Fifteen();
+                fifteen.methodfifteen();
+                break;
+            case 16:
+                Sixteen sixteen = new Sixteen();
+                sixteen.methodsixteen();
+                break;
+            case 17:
+                Seventeen factorial = new Seventeen();
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Введите число n: ");
+                int n = sc.nextInt();
+                factorial.factorial(n);
+                break;
+            case 18:
+                Eighteen dayOfTheWeek = new Eighteen();
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Введите номер дня недели: ");
+                int d = scanner.nextInt();
+                dayOfTheWeek.dayOfTheWeek(d);
+                break;
+            case 19:
+                Nineteen numberWord = new Nineteen();
+                Scanner scanner1 = new Scanner(System.in);
+                System.out.print("Введите число: ");
+                int num = scanner1.nextInt();
+                numberWord.numberWord(num);
+                break;
+
+        }
+    }
+}
